af2affce0c0f364aae27ca4188c4f84391043f09
KaziBadrulArif <161216047+KaziBadrulArif@users.noreply.github.com>
2025-04-12 23:04:24 -0600

    LifeisGood

--- job-portal-frontend/src/services/api.js
+++ job-portal-frontend/src/services/api.js
@@ -10,13 +10,11 @@ const api = axios.create({
   }
 });
 
-// Optional: If using JWT, uncomment the interceptor below
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('accessToken');
-//   if (token) {
-//     config.headers['Authorization'] = `Bearer ${token}`;
-//   }
-//   return config;
-// }, (error) => Promise.reject(error));
-
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 export default api;
